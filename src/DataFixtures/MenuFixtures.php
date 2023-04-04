bfc6b6cb3e3148433c2f3b11acf4cf0ccbbd7f29
Waflart Pierre <pierre.waflart@gmail.com>
2023-04-04 15:56:39 +0200

    add fixtures

--- src/DataFixtures/MenuFixtures.php
+++ src/DataFixtures/MenuFixtures.php
@@ -14,45 +14,121 @@ class MenuFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $ingredients = ['tomato', 'chess', 'speak'];
-        $menus = ['midi', 'enfant', 'soir'];
-        $plats = ['burger', 'pizza', 'glace'];
+        $ingredientsItems = ['tomate', 'champignon', 'emmental', 'jambon', 'chevre', 'merguez', 'olive'];
+        $ingredients = [];
 
-        $address = new Address();
-        $address->setName('12 rue lafayette');
-        $address->setZip(49100);
-        $address->setCity('Angers');
+        foreach ($ingredientsItems as $item) {
+            $ingredient = new Ingredient();
+            $ingredient->setName($item);
+            $ingredients[] = $ingredient;
+            $manager->persist($ingredient);
+        }
+
+        $address1 = new Address();
+        $address1->setName('12 place des oliviers');
+        $address1->setZip(75014);
+        $address1->setCity("Paris");
+
+        $address2 = new Address();
+        $address2->setName('1 square Alfred Musset');
+        $address2->setZip(75012);
+        $address2->setCity("Paris");
+
+        $restaurantsItems = [
+            [
+                "name" => "pizzeria Sicile",
+                "address" => $address1,
+                "director" => "Luigi",
+                "plats" => [
+                    'reine' => [
+                        'price' => 10,
+                        'ingredients' => [$ingredients[1], $ingredients[3], $ingredients[4]],
+                    ],
+                    'saumon' => [
+                        'price' => 11,
+                        'ingredients' => [$ingredients[2], $ingredients[5], $ingredients[1]],
+                    ],
+                    'chevres miel' => [
+                        'price' => 12,
+                        'ingredients' => [$ingredients[0], $ingredients[2], $ingredients[4]],
+                    ],
+                    'vegetariennes' => [
+                        'price' => 14,
+                        'ingredients' => [$ingredients[4], $ingredients[1], $ingredients[5]],
+                    ],
+                    'oriental' => [
+                        'price' => 15,
+                        'ingredients' => [$ingredients[3], $ingredients[2], $ingredients[6]],
+                    ]
+                ],
+                "menus" => [
+                    [
+                        "name" => "midi",
+                        "plat" => ["reine", 'saumon'],
+                        "price" => 19
+                    ]
+                ]
+            ],
+            [
+                "name" => "pizzeria Napoli",
+                "address" => $address2,
+                "director" => "Mario",
+                "plats" => [
+                    'reine' => [
+                        'price' => 10,
+                        'ingredients' => [$ingredients[5], $ingredients[3], $ingredients[2]],
+                    ],
+                    '4 fromages' => [
+                        'price' => 11,
+                        'ingredients' => [$ingredients[2], $ingredients[4], $ingredients[0]],
+                    ],
+                    'vegetariennes' => [
+                        'price' => 13,
+                        'ingredients' => [$ingredients[2], $ingredients[3], $ingredients[6]],
+                    ],
+                ],
+                "menus" => [
+                    [
+                        "name" => "midi",
+                        "plat" => ["reine", '4 fromages'],
+                        "price" => 21
+                    ]
+                ]
+
+            ]
+        ];
 
 
-        $restaurant = new Restaurant();
-        $restaurant->setName('Pizza del arte');
-        $restaurant->setDirector('Pierre');
-        $restaurant->setAddress($address);
-
-        foreach ($menus as $key => $menu) {
-            $cart = new Menu();
-            $cart->setName($menu);
-            $cart->setPrice(12);
-            $cart->setRestaurant($restaurant);
-
-            $plat = new Plat();
-            $plat->setRestaurant($restaurant);
-            $plat->setPrice(5);
-            $plat->setName($plats[$key]);
-
-            foreach ($ingredients as $ingredient) {
-                $ing = new Ingredient();
-                $ing->setName($ingredient);
-                $manager->persist($ing);
-
-                $plat->addIngredient($ing);
+        foreach ($restaurantsItems as $item) {
+            $restaurant = new Restaurant();
+            $restaurant->setName($item["name"]);
+            $restaurant->setAddress($item['address']);
+            $restaurant->setDirector($item['director']);
+            foreach ($item['plats'] as $name => $platItem) {
+                $plat = new Plat();
+                $plat->setName($name);
+                $plat->setPrice($platItem['price']);
+                foreach ($platItem['ingredients'] as $ingredientItem) {
+                    $plat->addIngredient($ingredientItem);
+                }
+                $restaurant->addPlat($plat);
+                $manager->persist($plat);
+                $manager->persist($restaurant);
             }
-            $manager->persist($restaurant);
-            $manager->persist($plat);
-            $cart->addPlat($plat);
-
-            $manager->persist($cart);
-
+            foreach ($item['menus'] as $menuItem) {
+                $menu = new Menu();
+                $menu->setName($menuItem['name']);
+                $menu->setPrice($menuItem['price']);
+                foreach ($menuItem['plat'] as $itemMenuPlat) {
+                    foreach ($restaurant->getPlats() as $platResto) {
+                        if($platResto->getName() === $itemMenuPlat){
+                            $menu->addPlat($platResto);
+                        }
+                    }
+                }
+                $restaurant->addMenu($menu);
+                $manager->persist($menu);
+            }
         }
 
         $manager->flush();
